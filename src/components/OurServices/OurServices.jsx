17e53210722483bec10d845dc075b4eb234334b8
Sergeyt2000 <sergeyt2000@gmail.com>
2026-03-30 16:55:45 +0300

    Fix text in services

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -5,29 +5,30 @@ import injectors from "../../assets/images/ServiceCard/injectors.jpg";
 
 export default function OurServices() {
   const decsriptionTNVD =
-    "Виконуємо професійну діагностику та ремонт ПНВТ (Common rail) з точним дотриманням технологічних стандартів. На сучасному спеціалізованому обладнанні дозволяють швидко та якісно виконувати роботи.";
+    "Виконуємо професійну діагностику та ремонт ПНВТ (Common rail) з точним дотриманням технологічних стандартів. На сучасному спеціалізованому обладнанні яке дозволяє швидко та якісно виконувати роботи.";
   const decsriptionInjectors =
     "Спеціалізуємось на обслуговуванні паливних систем легкових, вантажних автомобілів та спецтехніки. Пропонуємо швидку діагностику та професійний ремонт форсунок з виконанням усіх робіт протягом одного робочого дня.";
   return (
     <section className={css.ourServices}>
       <div className={css.container}>
-          <h2 className={css.title}>Пропонуємо (Здійснюємо, Робимо)</h2>
-          <ul className={css.servicesList}>
-            <li className={css.serviceItem}>
-              <ServiceCard
-                url={injectors}
-                alt="Ремонт форсунок"
-                description={decsriptionInjectors}
-              />
-            </li>
-            <li className={css.serviceItem}>
-              <ServiceCard
-                url={tnvd}
-                alt="Ремонт ПНВТ"
-                description={decsriptionTNVD}
-              />
-            </li>
-          </ul>
+        <h2 className={css.title}>Пропонуємо (Здійснюємо, Робимо)</h2>
+        <ul className={css.servicesList}>
+          <li className={css.serviceItem}>
+            <ServiceCard
+              url={injectors}
+              alt="Ремонт форсунок"
+              description={decsriptionInjectors}
+            />
+          </li>
+          <li className={css.serviceItem}>
+            <ServiceCard
+              url={tnvd}
+              alt="Ремонт ПНВТ"
+              alt2="(Паливний насос високого тиску)"
+              description={decsriptionTNVD}
+            />
+          </li>
+        </ul>
       </div>
     </section>
   );
--- src/components/OurServices/ServiceCard/ServiceCard.jsx
+++ src/components/OurServices/ServiceCard/ServiceCard.jsx
@@ -1,13 +1,14 @@
 import css from "./ServiceCard.module.css";
 
-export default function ServiceCard({ url, alt, description }) {
-    return (
-      <div className={css.serviceCard}>
-        <img src={url} alt={alt} />
-        <div className={css.content}>
-            <h3 className={css.title}>{alt}</h3>
-            <p className={css.description}>{description}</p>
-        </div>
+export default function ServiceCard({ url, alt, alt2, description }) {
+  return (
+    <div className={css.serviceCard}>
+      <img src={url} alt={alt} />
+      <div className={css.content}>
+        <h3 className={css.title}>{alt}</h3>
+        <p className={css.description}>{alt2}</p>
+        <p className={css.description}>{description}</p>
       </div>
-    );
+    </div>
+  );
 }
