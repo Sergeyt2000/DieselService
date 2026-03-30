3b3b68fc5cd5b36bb785d894e9a6f9aaa25a37f5
Sergeyt2000 <sergeyt2000@gmail.com>
2026-03-30 14:43:58 +0300

    Fix burger menu styles

--- src/components/Header/BurgerMenu/BurgerMenu.jsx
+++ src/components/Header/BurgerMenu/BurgerMenu.jsx
@@ -2,15 +2,17 @@ import css from "./BurgerMenu.module.css";
 
 export default function BurgerMenu({ open, setOpen }) {
   return (
-    <button
-      type="button"
-      className={`${css.burger} ${open ? css.open : ""}`}
-      onClick={() => setOpen(!open)}
-      aria-label={open ? "Закрити меню" : "Відкрити меню"}
-    >
-      <span className={css.line}></span>
-      <span className={css.line}></span>
-      <span className={css.line}></span>
-    </button>
+    <div className={`${css.burgerContainer} ${open ? css.open : ""}`}>
+      <button
+        type="button"
+        className={`${css.burger} ${open ? css.open : ""}`}
+        onClick={() => setOpen(!open)}
+        aria-label={open ? "Закрити меню" : "Відкрити меню"}
+      >
+        <span className={css.line}></span>
+        <span className={css.line}></span>
+        <span className={css.line}></span>
+      </button>
+    </div>
   );
 }
