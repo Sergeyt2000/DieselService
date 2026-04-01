dc81f3821d12db939a11607b1441afe56be8a6ee
Sergeyt2000 <sergeyt2000@gmail.com>
2026-04-01 10:50:19 +0300

    Add after on active page

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,29 +1,48 @@
 import css from "./Navigation.module.css";
+import { useLocation } from "react-router-dom";
 
 export default function Navigation({ isMobile = false, onLinkClick }) {
+  const location = useLocation();
   const handleClick = (e) => {
     if (onLinkClick) {
       onLinkClick(); // закриваємо меню після кліку
     }
   };
-
+const isActive = (path) => {
+  if (path === "/") {
+    return location.pathname === "/";
+  }
+  return location.pathname.startsWith(path);
+};
   return (
     <nav
       className={`${css.navigation} ${isMobile ? css.mobileNav : css.desktopNav}`}
     >
       <ul className={css.list}>
         <li className={css.item}>
-          <a href="/" onClick={handleClick}>
+          <a
+            href="/"
+            onClick={handleClick}
+            className={isActive("/") ? css.active : ""}
+          >
             Головна
           </a>
         </li>
         <li className={css.item}>
-          <a href="/about" onClick={handleClick}>
+          <a
+            href="/about"
+            onClick={handleClick}
+            className={isActive("/about") ? css.active : ""}
+          >
             Про нас
           </a>
         </li>
         <li className={css.item}>
-          <a href="/contacts" onClick={handleClick}>
+          <a
+            href="/contacts"
+            onClick={handleClick}
+            className={isActive("/contacts") ? css.active : ""}
+          >
             Контакти
           </a>
         </li>
